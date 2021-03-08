35c7023e1e38bf8bdc8151ddb23cd300a8b2bd14
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2021-03-08 16:30:11 +0500

    Add language model

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 from django.urls import reverse
 import uuid
 
@@ -16,6 +17,7 @@ class Book (models.Model):
     isbn = models.CharField("ISBN", max_length=13, unique=True,
                             help_text= '13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -67,3 +69,10 @@ class Author(models.Model):
         return f'{self.last_name},{self.first_name}'
 
 
+class Language(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+    def __str__(self):
+        return self.name
+
+    
